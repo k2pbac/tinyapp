21baf4140b245f9b1eb026cd00202674df37764c
k2pbac <krisbachan123@hotmail.com>
2021-08-30 15:19:58 -0400

    add redirect to long url when clicking shorturl

--- express_server.js
+++ express_server.js
@@ -25,9 +25,8 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+
 app.get("/urls/new", (req, res) => {
-
-
   res.render('urls_new');
 });
 
@@ -51,7 +50,10 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 
-
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
