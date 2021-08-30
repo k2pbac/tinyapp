a4bbed28d249142862adb4d734e34a30b617d461
k2pbac <krisbachan123@hotmail.com>
2021-08-30 13:25:59 -0400

    add new urls/id route with starter html

--- express_server.js
+++ express_server.js
@@ -15,16 +15,18 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  res.render('urls_index');
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls/:shortURL", (req, res) => {
+
+  const { shortURL } = req.params;
+  const longURL = urlDatabase[shortURL]
+
+  res.render('urls_show', {shortURL, longURL});
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
