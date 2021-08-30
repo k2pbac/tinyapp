c6df527d0afae46e8eb994f9451b34d53a6a4525
k2pbac <krisbachan123@hotmail.com>
2021-08-30 15:41:20 -0400

    add flash and session to file, add error catching partial

--- express_server.js
+++ express_server.js
@@ -2,13 +2,37 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const {generateRandomString} = require("./generateRandomString");
-const utf8 = require("utf8");
+const flash = require('connect-flash');
+const session = require('express-session');
 
 app.use(express.urlencoded({
   extended: true
 }));
 app.set("view engine", "ejs");
 
+//Create a session config and call both flash and session to use methods
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+const sessionConfig = {
+  name: 'session',
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+app.use(flash());
+app.use(session(sessionConfig));
+
+//Set locals for success and error to use throughout file
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -37,7 +61,6 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
 
   urlDatabase[shortURL] = longURL;
-  console.log(urlDatabase);
   res.redirect("urls");
 });
 
@@ -63,8 +86,8 @@ app.get("/u/:shortURL", (req, res, next) => {
 });
 
 app.get('*', (req, res) => {
-
-  res.send("sorry page doesn't exist");
+  req.flash('error', 'Sorry, page was not found!');
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
